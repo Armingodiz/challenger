06dc94883d703ec3d6f13a8f361af862a9732cbb
ArminGodiz <armingodarzi1380@gmail.com>
2021-07-12 19:35:30 +0430

    reader package implemented

---
+++ pkg/reader/reader.go
@@ -0,0 +1,15 @@
+package reader
+
+import (
+	"github.com/ArminGodiz/golang-code-challenge/pkg/broker"
+	"github.com/ArminGodiz/golang-code-challenge/pkg/models"
+)
+
+type Reader struct {
+	Broker broker.Broker
+}
+
+func (reader *Reader) StartReading(inputChannel chan models.BrokerData) {
+	reader.Broker.Consume(inputChannel)
+}
+
