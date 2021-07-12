5de00a2aac37f2cfc02b00cbe9e925896af9b8c4
ArminGodiz <armingodarzi1380@gmail.com>
2021-07-12 13:19:57 +0430

    BrokerData model added

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -2,7 +2,7 @@ package models
 
 type BrokerData struct {
 	UserName     string `json:"user_name"`
-	Id           int    `json:"id"`
+	ID           int    `json:"id"`
 	TrafficUsage int    `json:"traffic_usage"`
 	Ip           string `json:"ip"`
 	Port         string `json:"port"`
