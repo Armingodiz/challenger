405c7b4ce2fffcf99e6a08579b22253225aec6bc
ArminGodiz <armingodarzi1380@gmail.com>
2021-07-13 00:23:23 +0430

    combiner package implemented

---
+++ pkg/combiner/combiner.go
@@ -0,0 +1,35 @@
+package combiner
+
+import (
+	pkgCache "github.com/ArminGodiz/golang-code-challenge/pkg/cache"
+	"github.com/ArminGodiz/golang-code-challenge/pkg/models"
+)
+
+type Combiner struct {
+	GoRoutinesCapacity int
+	InputChannel       chan models.BrokerData
+	OutputChannel      chan models.CsvData
+	Cache              pkgCache.CacheInterface
+}
+
+func GetCombiner(goRoutinesCap int, inputChannel chan models.BrokerData, outputChannel chan models.CsvData) *Combiner {
+	return &Combiner{
+		GoRoutinesCapacity: goRoutinesCap,
+		InputChannel:       inputChannel,
+		OutputChannel:      outputChannel,
+	}
+}
+
+func (combiner *Combiner) StartCombining() {
+	pkgCache.SetCacheClient(8282)
+	combiner.Cache = pkgCache.CacheObject
+	for i := 0; i < combiner.GoRoutinesCapacity; i++ {
+		go func() {
+			for brokerData := range combiner.InputChannel {
+				//time.Sleep(3 * time.Second)
+				mac := combiner.Cache.Get(brokerData.Ip)
+				combiner.OutputChannel <- models.CsvData{BrokerInfo: brokerData, Mac: mac }
+			}
+		}()
+	}
+}
