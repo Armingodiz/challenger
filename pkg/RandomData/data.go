8447768c13929b2f10d419bf4ba02d475343113c
ArminGodiz <armingodarzi1380@gmail.com>
2021-07-12 13:54:39 +0430

    randomData pkg added to create 200 random data ffor broker publisher to send

---
+++ pkg/RandomData/data.go
@@ -0,0 +1,61 @@
+package RandomData
+
+import (
+	crand "crypto/rand"
+	"fmt"
+	"github.com/ArminGodiz/golang-code-challenge/pkg/models"
+	"math/rand"
+	"time"
+)
+
+var DataBroker []models.BrokerData
+var DataCache []models.CacheData
+
+func init() {
+	DataBroker = make([]models.BrokerData, 200)
+	DataCache = make([]models.CacheData, 200)
+	for i := 0; i < 200; i++ {
+		min, max := getMinMax(i % 4)
+		data := models.BrokerData{
+			UserName:     getName(4),
+			ID:           1000 + i,
+			TrafficUsage: rand.Intn((max - min) + min),
+			Ip:           genIpAddr(),
+			Port:         "8080",
+		}
+		DataBroker = append(DataBroker, data)
+		DataCache = append(DataCache, models.CacheData{Ip: data.Ip, Mac: getMacAdd()})
+	}
+}
+
+func getMacAdd() string {
+	buf := make([]byte, 6)
+	_, err := crand.Read(buf)
+	if err != nil {
+		fmt.Println("error:", err)
+		return ""
+	}
+	buf[0] |= 2
+	return fmt.Sprintf("Random MAC address: %02x:%02x:%02x:%02x:%02x:%02x\n", buf[0], buf[1], buf[2], buf[3], buf[4], buf[5])
+}
+
+func genIpAddr() string {
+	rand.Seed(time.Now().Unix())
+	ip := fmt.Sprintf("%d.%d.%d.%d", rand.Intn(255), rand.Intn(255), rand.Intn(255), rand.Intn(255))
+	return ip
+}
+
+func getMinMax(n int) (int, int) {
+	switch n {
+	case 0:
+		return 0, 100
+	case 1:
+		return 101, 500
+	case 2:
+		return 501, 1000
+	case 3:
+		return 1001, 1500
+	default:
+		return 0, 0
+	}
+}
