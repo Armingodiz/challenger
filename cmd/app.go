0fec26fc5b309f922ced0dd07598bcbb6bbad08a
ArminGodiz <armingodarzi1380@gmail.com>
2021-07-13 17:00:36 +0430

    writer part added

--- cmd/app.go
+++ cmd/app.go
@@ -5,16 +5,20 @@ import (
 	pkgCombiner "github.com/ArminGodiz/golang-code-challenge/pkg/combiner"
 	"github.com/ArminGodiz/golang-code-challenge/pkg/models"
 	pkgReader "github.com/ArminGodiz/golang-code-challenge/pkg/reader"
+	pkgWriter "github.com/ArminGodiz/golang-code-challenge/pkg/writer"
 )
 
 func StartApplication() {
 	brokerChannel := make(chan models.BrokerData, 200)
 	combinerChannel := make(chan models.CsvData, 200)
+	writerChannel := make(chan models.CsvData, 200)
 	reader := &pkgReader.Reader{}
 	combiner := pkgCombiner.GetCombiner(4, brokerChannel, combinerChannel)
+	writer := pkgWriter.GetNewWriter(2, combinerChannel, writerChannel)
 	go reader.StartReading(brokerChannel)
 	go combiner.StartCombining()
-	for combined := range combinerChannel {
-		fmt.Println(combined.Mac)
+	go writer.StartWriting()
+	for result := range writerChannel {
+		fmt.Println(result)
 	}
 }
