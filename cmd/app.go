763aa87f335c9413ac5534cf4b1acac108f71395
ArminGodiz <armingodarzi1380@gmail.com>
2021-07-14 01:23:12 +0430

    config part added

--- cmd/app.go
+++ cmd/app.go
@@ -9,19 +9,35 @@ import (
 )
 
 func StartApplication() {
-	redisPort := 8282
+	//set config
+	config := GetConfig()
+	redisPort := config.RedisPort
+	goroutinesCount := config.GoroutinesCount
+	combinerRoutines, writerRoutines := setGoroutines(goroutinesCount)
+	//create channels
 	brokerChannel := make(chan models.BrokerData, 200)
 	combinerChannel := make(chan models.CsvData, 200)
 	writerChannel := make(chan []string, 200)
+	//create each part
 	reader := &pkgReader.Reader{}
-	combiner := pkgCombiner.GetCombiner(4, brokerChannel, combinerChannel)
-	writer := pkgWriter.GetNewWriter(9, combinerChannel, writerChannel)
+	combiner := pkgCombiner.GetCombiner(combinerRoutines, brokerChannel, combinerChannel)
+	writer := pkgWriter.GetNewWriter(writerRoutines, combinerChannel, writerChannel)
+	//start each part in go routine
 	go reader.StartReading(brokerChannel)
 	go combiner.StartCombining(redisPort)
 	go writer.StartWriting()
+	//waiting in main goRoutine for response from last part(writer)
 	for result := range writerChannel {
 		if result != nil {
 			fmt.Println("part written on file")
 		}
+	}
+}
+
+func setGoroutines(count int) (int, int) {
+	if (count-1)%2 == 0 {
+		return (count - 1) / 2, (count - 1) / 2
+	} else {
+		return (count)/2 - 1, count / 2
 	}
 }
---
+++ cmd/config.go
@@ -0,0 +1,24 @@
+package cmd
+
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+type Config struct {
+	GoroutinesCount int `json:"goroutines_count"`
+	RedisPort       int `json:"redis_port"`
+}
+
+func GetConfig() Config {
+	jsonFile, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		panic(err)
+	}
+	var config Config
+	err = json.Unmarshal([]byte(jsonFile), &config)
+	if err != nil {
+		panic(err)
+	}
+	return config
+}
