97047dc79fbc40a77e948fbfef71850e2f656feb
ArminGodiz <armingodarzi1380@gmail.com>
2021-07-14 13:07:14 +0430

    change in CombinerMock, now cahceMock is not needed

--- pkg/combiner/mockCombiner.go
+++ pkg/combiner/mockCombiner.go
@@ -13,12 +13,10 @@ type CombinerMock struct {
 }
 
 func (combiner *CombinerMock) StartCombining(port int) {
-	combiner.Cache = pkgCache.GetMockCache()
 	for i := 0; i < combiner.GoRoutinesCapacity; i++ {
 		go func() {
 			for brokerData := range combiner.InputChannel {
-				//time.Sleep(3 * time.Second)
-				mac := combiner.Cache.Get(brokerData.Ip)
+				mac := ""
 				combiner.OutputChannel <- models.CsvData{BrokerInfo: brokerData, Mac: mac}
 			}
 		}()
